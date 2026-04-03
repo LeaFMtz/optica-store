9a375d6e0ba31be75d7387c089c25fd540d33e48
Gustavo Galvan <ggalvan2712@outlook.com>
2026-04-03 06:15:24 -0300

    Correccion en el ignore

--- resources/views/components/header/search.blade.php
+++ resources/views/components/header/search.blade.php
@@ -1,26 +1,18 @@
 <form {{ $attributes->merge(['class' => 'w-full relative']) }}
-      action="{{ route('search.view') }}">
-    <input name="term"
-           type="search"
-           autocomplete="off"
-           data-lpignore="true"
-           data-bitwarden-ignore
-           placeholder="¿Qué estás buscando?"
-           class="w-full pl-6 pr-12 py-3 text-sm bg-black text-white border border-white rounded-full focus:ring-[#71C229] focus:border-[#71C229] placeholder-gray-400"
-           value="{{ $this->term }}" />
+	action="{{ route('search.view') }}">
+	<input name="term" type="search" autocomplete="off" data-lpignore="true" data-bitwarden-ignore
+		placeholder="¿Qué estás buscando?"
+		class="w-full pl-6 pr-12 py-3 text-sm bg-black text-white border border-white rounded-full focus:ring-[#71C229] focus:border-[#71C229] placeholder-gray-400"
+		value="{{ $this->term }}" />
 
-    <button class="absolute p-2 text-white transition -translate-y-1/2 rounded-full right-3 top-1/2 hover:text-[#71C229]">
-        <span class="sr-only">Submit Search</span>
+	<button
+		class="absolute p-2 text-white transition -translate-y-1/2 rounded-full right-3 top-1/2 hover:text-[#71C229]">
+		<span class="sr-only">Submit Search</span>
 
-        <svg xmlns="http://www.w3.org/2000/svg"
-             class="w-6 h-6"
-             fill="none"
-             viewBox="0 0 24 24"
-             stroke="currentColor">
-            <path stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-        </svg>
-    </button>
+		<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
+			stroke="currentColor">
+			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+				d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+		</svg>
+	</button>
 </form>
