f47cab2df4540495f3af6c0fdbe901887d17b374
Gustavo Galvan <ggalvan2712@outlook.com>
2026-05-03 20:26:16 -0300

    correccion de mirgations

--- database/migrations/2026_04_03_002506_create_banners_table.php
+++ database/migrations/2026_04_03_002506_create_banners_table.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Lunar\Base\Migration;
 
 return new class extends Migration
 {
@@ -13,10 +13,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('banners', function (Blueprint $table) {
+        Schema::create($this->prefix.'banners', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('image_path');
             $table->string('url')->nullable();
             $table->string('position')->default('home_hero');
             $table->integer('order')->default(0);
@@ -30,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('banners');
+        Schema::dropIfExists($this->prefix.'banners');
     }
 };
