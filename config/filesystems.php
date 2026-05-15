618551d11d01a2cdac30be40648eb8533a51fe54
Gustavo Galvan <ggalvan2712@outlook.com>
2026-05-15 04:08:24 -0300

    Correccion en el disco de media

--- config/filesystems.php
+++ config/filesystems.php
@@ -67,6 +67,7 @@ return [
             'url' => env('AWS_URL').'/media',
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'root' => 'media',
             'throw' => false,
         ],
 
